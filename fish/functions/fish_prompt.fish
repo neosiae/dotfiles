5ab7abb581a5a48d3b00576bd4515b0d815da1b3
neosiae <neosiae@gmail.com>
2018-08-23 15:25:22 +0200

    Add custom prompt

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,12 @@
+function fish_prompt -d "Write out the prompt"
+  set_color blue 
+  printf '\n[%s]' (basename $PWD)
+
+  set_color magenta 
+  printf ' %s' (git_branch_name)
+  
+  set_color $fish_color_cwd 
+  printf '\n%s~$ ' (whoami)
+
+  set_color normal
+end
